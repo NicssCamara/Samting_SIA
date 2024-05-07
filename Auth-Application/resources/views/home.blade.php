52932d99842c415b15d8678bc4a4db267ee8105e
Nics-IT2R2 <160290934+Nics-IT2R2@users.noreply.github.com>
2024-05-07 20:00:47 +0800

    new files

---
+++ Auth-Application/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.user')
+ 
+@section('title', 'Home')
+ 
+@section('contents')
+<body class="leading-normal tracking-wider bg-gradient-to-b from-blue-500 to-sky-500">
+
+<div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto">
+    <div class="flex-auto max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <h2 class="text-5xl h-15 font-bold text-blue-800 text-center">Authenty<span class="hidden sm:inline text-white">  is now here to help on your journey!</h2>
+    
+        <p class="pt-10 text-sm h-15 font-semibold text-blue-800 text-center">What can we offer?<span class="hidden sm:inline font-bold"> MORAL SUPPORT!!!</p>
+        <p class="text-sm h-15 font-semibold text-blue-800 text-center">hehe we also need help T__T</p>
+
+    </div>
+    </div>
+
+</body>
+@endsection
