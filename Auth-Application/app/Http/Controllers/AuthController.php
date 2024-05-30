b59724bb90dd9e7a5b025ebe4b3cbc17c42b615c
jofafs <160290783+jofafs@users.noreply.github.com>
2024-05-30 20:38:32 +0800

    rosary time
    mama fe will ult now

--- Auth-Application/app/Http/Controllers/AuthController.php
+++ Auth-Application/app/Http/Controllers/AuthController.php
@@ -1,83 +1,94 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
- 
+use App\Http\Requests\RegisterRequest;
+
 class AuthController extends Controller
 {
- 
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
- 
+
     public function register()
     {
         return view('auth/register');
     }
- 
-    public function registerSave(Request $request)
+
+    public function registerSave(RegisterRequest $request)
     {
-        Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|confirmed'
-        ])->validate();
- 
-        User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
             'type' => "0"
         ]);
- 
-        return redirect()->route('login');
+
+
+        Auth::login($user);
+
+        return redirect()->route('home');
     }
- 
+
     public function login()
     {
         return view('auth/login');
     }
- 
+
     public function loginAction(Request $request)
     {
-        Validator::make($request->all(), [
-            'email' => 'required|email',
-            'password' => 'required'
-        ])->validate();
- 
-        if (!Auth::attempt($request->only('email', 'password'), $request->boolean('remember'))) {
+        $this->validateLogin($request);
+
+        if (!Auth::attempt($this->credentials($request), $request->boolean('remember'))) {
             throw ValidationException::withMessages([
                 'email' => trans('auth.failed')
             ]);
         }
- 
+
         $request->session()->regenerate();
- 
+
         if (auth()->user()->type == 'admin') {
             return redirect()->route('admin/home');
         } else {
             return redirect()->route('home');
         }
-         
+
         return redirect()->route('dashboard');
     }
- 
+
+    protected function credentials(Request $request)
+    {
+        $email = filter_var($request->email, FILTER_SANITIZE_EMAIL);
+        $password = $request->password;
+
+        return ['email' => $email, 'password' => $password];
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+    }
+
     public function logout(Request $request)
     {
         Auth::guard('web')->logout();
- 
+
         $request->session()->invalidate();
- 
+
         return redirect('/login');
     }
- 
+
     public function profile()
     {
         return view('userprofile');
